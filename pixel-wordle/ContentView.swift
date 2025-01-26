826dc9489d7a030d65620c7cc0f1784dfbd6fcfd
nigelnh <121311174+nigelnh@users.noreply.github.com>
2025-01-26 15:56:22 -0500

    Initial Commit

---
+++ pixel-wordle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pixel-wordle
+//
+//  Created by Nhân Nguyễn on 1/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pixel-wordle/pixel_wordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  pixel_wordleApp.swift
+//  pixel-wordle
+//
+//  Created by Nhân Nguyễn on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct pixel_wordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
